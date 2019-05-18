2fd9bebcebc944a77e8f208f518f40a2a0613464
glendaqm <50159174+glendaqm@users.noreply.github.com>
2019-05-17 23:37:12 -0600

    Tarea 5 comienzo

---
+++ tarea_5/tarea_5.py
@@ -0,0 +1,32 @@
+'''
+
+identificación
+Nombre
+Apellido
+teléfono
+dirección
+lista de enfermedades tratadas
+lista de medicamentos que toma
+
+'''
+
+listaPacientes = {
+    "id":1234, "nombre": 'Glenda', "apellido":'Quesada',"tel":8311,"dir":'Alajuela',"enfermedades":'demencia y presion alta', "medicamentos":'naturales'
+  }
+
+'''
+Operaciones:
+
+Ingreso de un paciente nuevo
+Borrado de un paciente
+Agregar más enfermedades en un paciente en particular
+Agregar más medicamentos en un paciente en particular
+Generar reporte de las enfermedades tratadas en la clínica
+Generar reporte de los medicamentos entregados en la clínica
+Comparar 2 pacientes en particular: cuales enfermedades tienen en común. Cuales no?. Lo mismo con los medicamentos.
+Consideraciones:
+
+La lista inicial de pacientes puede estar escrito en el código asi como su información. 
+La implementación puede ser abierta: puede ser que cada paciente sea creado por una clase o 
+simplemente cada paciente sea un diccionario con toda la información solicitada.
+'''
