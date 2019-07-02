360057afb2f23721c0867309f26513b4672eaa6f
glendaqm <50159174+glendaqm@users.noreply.github.com>
2019-07-01 23:33:38 -0600

    Projecto Curso - calculo salario neto arreglado y agregados todos los comentarios para la exposicion

--- Projecto/ProjectoCursoDatos.py
+++ Projecto/ProjectoCursoDatos.py
@@ -1,85 +1,95 @@
+# importamos la libreria para hacer mailmerge (adicion automatica de nombres y direcciones de una DB a cartas, sobres para facilitar el envio de correo especialmente masivo
 from __future__ import print_function
 from mailmerge import MailMerge
-#from datetime import date
 
-
+#libreria para construir y manejar HTTP requests
 import requests
+#esta libreria es para convertir una cifra en numeros a letras
 import num2words
 #import json
 
 
 
-#Funcion llamada del API
+#Creamos una Funcion para llamar al web service
 
-#pedir al usuario la cedula del empleado para generar la constancia
+#pedir al usuario que digite la cedula del empleado para generar la constancia
 ced = input("Digite la cédula del empleado: ")
 print(ced)
 
-#Llamar al web service y pasar la cedula digitada por parametro
+# Creamos un HttP request (get) para invocar al web service y pasar la cedula digitada como parametro del request
+# Recordemos que el web service esta esperando el metodo get
 datos = {'cedula': ced}
 r = requests.get('http://127.0.0.1:5000/api/v1/resources/emp', params=datos)
 
-#imprimir el status code del response
+#imprimir el status code del response, si fue exitoso imprime un 200
 print(r.status_code)
 
 #se crea un diccionario con el json que viene del response para la cedula consultada
 dict_emp = {}
-#esto lo que va a hacer es quitar el primer
+
+#esto lo que va a hacer es quitar el primer elemento del diccionario para que solo quede la info que viene del web service
 dict_emp = r.json()[-1]
+
+# imprimimos el contenido para ver que informacion nos trajo
 print(dict_emp)
 
-
+# guardamos el template de la constancia en la variable template
 template = "Constancia_salarial_template.docx"
 
-#To create a mailmerge document and look at all of the fields:
+#Creamos un documento mailmerge que contiene la informacion del template
 document = MailMerge(template)
+#imprimimos el contenido del objeto para ver todos los campos a llenar que contiene
 print(document.get_merge_fields())
-
-
 
 
 #Funcion para insertar info en Doc de word
 
-#SalarioNetoLetras = num2words(SalarioNeto,lang=es)
 
+
+
+#Funcion para calcular el salario Neto, recibe el salario bruto por parametro
+#calculo de renta:
 #817 - 1,226: 10%
 #1,226 - 2,103: 15%
 #2,103 - 4,205: 20%
 #4,205+ : 25%
 
-
-'''
 def calcSalarioNeto(salarioBruto) :
     salarioNeto = 0
+    caja = salarioBruto * 0.1034
     renta = 0
-    caja = salarioBruto = (salarioBruto * 10.34)
-    if (salarioBruto <= 817,000) :
+    if (salarioBruto <= 817000) :
         renta = renta
-    elif (salarioBruto > 817,000 and salarioBruto <= 1,226,000) :
-        renta = renta + ((salarioBruto - 817,00) * 0.10)
-    elif (salarioBruto > 1,226,000 and salarioBruto <= 2,103,000):
-        renta = renta + ((salarioBruto - 1,226,000) * 0.15)
-    elif (salarioBruto > 2,103,000 and salarioBruto <= 4,205,000):
-        renta = renta + ((salarioBruto - 2,103,000) * 0.20)
     else :
-        renta = renta + ((salarioBruto - 4,205,000)* 0.25)
+        renta = (1226000 - 817000) * 0.10
+        if (salarioBruto > 1226000):
+            if (salarioBruto <= 2103000):
+                renta = renta + ((salarioBruto - 1226000) * 0.15)
+            else: # mayor a 2,103,000
+                renta = renta + ((2103000 - 1226000) * 0.15)
+                if (salarioBruto <= 4205000):
+                    renta = renta + ((salarioBruto - 2103000) * 0.20)
+                else : # mayor a 4,205,000
+                    renta = renta + ((4205000 - 2103000) * 0.20)
+                    renta = renta + ((salarioBruto - 4205000)* 0.25)
 
     salarioNeto = salarioBruto - caja - renta
     return salarioNeto
 
-'''
 
-dict_emp['salarioNeto']= 50000
-#dict_emp['salarioNeto']=calcSalarioNeto(dict_emp['salarioBruto'])
 
+# Llamamos a la funcion que calcula el salario Neto y guardamos el valor que devuelve en el diccionario
+dict_emp['salarioNeto']=calcSalarioNeto(dict_emp['salarioBruto'])
+
+# Guardamos la version en letras del salario Neto y Bruto
 netoLetras = num2words.num2words(dict_emp['salarioNeto'],lang='es')
 brutoLetras = num2words.num2words(dict_emp['salarioBruto'],lang='es')
 
-print('bruto', brutoLetras)
 
-print(document.get_merge_fields())
+#print(' salario neto', netoLetras)
+#print(' salario bruto', brutoLetras)
 
-
+#con la funcion merge vamos a asignarle un valor a cada campo que hay que rellenar en la constancia
 document.merge(
 
     Cedula=str(dict_emp['cedula']),
@@ -88,22 +98,12 @@ document.merge(
     Puesto=dict_emp['puesto'],
     NombreEmpleado=dict_emp['nombre']+' '+dict_emp['apellido1']+' '+dict_emp['apellido2'],
     FechaIngreso=dict_emp['fecha_ingreso'],
-    #calcSalarioNeto(dict_emp['salarioBruto']),
     SalarioNeto= str(dict_emp['salarioNeto']),
     SalarioNetoLetras=str(netoLetras) + ' colones',
     SalarioBrutoLetras=str(brutoLetras)+' colones'
 )
 
-
+#creamos un archivo usando el template y la informacion del empleado que insertamos
 document.write('test-output.docx')
 
-
-#Un dataframe no es tan efectivo como un dictionario para obtener los valores y pasarlos al word
-
-#Convert a JSON string to pandas object with read_json
-
-#esto si
-#df3 = pd.DataFrame(pd.read_json(info_emp, typ='series').to_frame())
-
-#print(df3.head())
 
--- Projecto/Proyecto_API2.py
+++ Projecto/Proyecto_API2.py
@@ -1,14 +1,20 @@
+#importamos una libreria o web framework para crear web applications y APIs. Hay otras como Django y CherryPy
 import flask
 from flask import request, jsonify
 
-#Creates the Flask application object, which contains data about the application and also methods (object functions) that tell the application to do certain actions.
-# The last line, app.run(), is one such method.
+#Creamos el objeto applicación de tipo Flask para crear nuestro web application (o web service),
+# que contiene información acerca de la aplicación y también métodos (funciones)
+#que le dicen a la aplicación que haga ciertas cosas, como la función "run"
+
 app = flask.Flask(__name__)
-#Starts the debugger. With this line, if your code is malformed, you’ll see an error when you visit your app.
+#Le decismos que nos deje hacer "debugging" para que podamos ver errores cuando ejecutemos el
+# código de la aplicación
 app.config["DEBUG"] = True
 
 
-#Diccionario de empleados. En la vida real Workday lo obtendria de una base de datos
+#Creamos una lista de Diccionarios con la información de los empleados que vamos a necesitar para la constancia.
+# En la vida real Workday lo obtiene de una base de datos, no están los datos en caliente
+
 empleados = [
     {
         "cedula": 110870778,
@@ -21,14 +27,14 @@ empleados = [
         "salarioBruto":300000
     },
     {
-        "cedula": 110001000,
-        "nombre": "Juan",
-        "apellido1": "Perez",
-        "apellido2": "Perez",
-        "fecha_ingreso":"10 de Noviembre del 2002",
-        "puesto": "Sr. Programmer",
-        "departamento":"Information Services",
-        "salarioBruto":200000
+        "cedula": 114380418,
+        "nombre": "Cristina alias Titi",
+        "apellido1": "Penon",
+        "apellido2": "Portman",
+        "fecha_ingreso":"2 de Enero del 2009",
+        "puesto": "Analyst III",
+        "departamento":"EIM Reporting",
+        "salarioBruto":400000
     },
     {
         "cedula": 900010783,
@@ -38,7 +44,7 @@ empleados = [
         "fecha_ingreso":"16 de Junio del 2019",
         "puesto": "Asistente Administrativa",
         "departamento":"Recursos Humanos",
-        "salarioBruto":100000
+        "salarioBruto":5000000
     },
     {
         "cedula": 110250162,
@@ -52,37 +58,37 @@ empleados = [
     }
 ]
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return "<h1>404</h1><p>El recurso no pudo ser encontrado.</p>", 404
+#El proceso de mapear URLs a funciones se llama routing. La sintaxis @app.route('/', methods=['GET']) es la parte
+# del programa que le deja saber a Flask que esta función "api_empleado" se debe asociar al path: /api/v1/resources/emp
+# La lista methods (methods=['GET']) es un parametro de la funcion route que le permite a Flask saber qué tipo
+#de HTTP requests son permitidos. En este caso nos interesa hacer un Get (el get y especificar un path son parte de
+#las reglas del protocolo HTTP y no se python o de Flask)
 
-
-#The process of mapping URLs to functions is called routing. The @app.route('/', methods=['GET']) syntax is the part
-# of the program that lets Flask know that this function, home, should be mapped to the path /.
-# The methods list (methods=['GET']) is a keyword argument that lets Flask know what kind of HTTP requests are allowed.
 
 @app.route('/api/v1/resources/emp', methods=['GET'])
 def api_empleado():
-    # Check if an ID was provided as part of the URL.
-    # If ID is provided, assign it to a variable.
-    # If no ID is provided, display an error in the browser.
+    # Verifica si la cedula es parte del URL. Si la cédula es provista, la asigna a una variable para guardarla.
+    # Si no, despliega un error en el browser.
     if 'cedula' in request.args:
         cedula = int(request.args['cedula'])
     else:
         return "Error: La cédula no fue provista. Por favor especifique una."
 
-    # Create an empty list for our results
+    # Creamos una lista vacía llamada resultados, donde vamos a guardar la info
+    # del empleado si es encontrado por medio de la cédula
     results = []
 
-    # Loop through the data and match results that fit the requested ID.
-    # IDs are unique, but other fields might return many results
+    # Recorremos la lista empleados para ver si encontramos la cédula provista
     for empleado in empleados:
         if empleado['cedula'] == cedula:
             results.append(empleado)
+    # Si la cédula no hizo match con ningún registro entonces tira un error que el usuario entienda
+    if results== [] :
+        return "Error: La cédula provista no fue encontrada."
 
-    # Use the jsonify function from Flask to convert our list of
-    # Python dictionaries to the JSON format.
+    # Usamos la función jsonify de Flask para convertir nuestra lista de
+    # dictionarios de Python a formato JSON (generalmente los web services devuelven JSON o XML
     return jsonify(results)
 
-
+#con esta instrucción "levantamos" nuestro humilde web service
 app.run()
